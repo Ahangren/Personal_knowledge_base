53a3f926dd32e7abd070bd51ebc558f8b8599008
Ahangren <3495901744@qq.com>
2025-07-03 17:52:01 +0800

    文心千帆中的embedding

---
+++ embedding_procedure/WenXin_Embedding.py
@@ -0,0 +1,40 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv,find_dotenv
+
+
+def wenxin_embedding(text: str):
+    # 获取环境变量 wenxin_api_key、wenxin_secret_key
+    api_key = os.environ['QIANFAN_AK']
+    secret_key = os.environ['QIANFAN_SK']
+
+    # 使用API Key、Secret Key向https://aip.baidubce.com/oauth/2.0/token 获取Access token
+    url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={0}&client_secret={1}".format(
+        api_key, secret_key)
+    payload = json.dumps("")
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    # 通过获取的Access token 来embedding text
+    url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/embeddings/embedding-v1?access_token=" + str(
+        response.json().get("access_token"))
+    input = []
+    input.append(text)
+    payload = json.dumps({
+        "input": input
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return json.loads(response.text)
+
+if __name__ == '__main__':
+    text='这里是文心大模型embedding模块'
+    response=wenxin_embedding(text)
